73f85be9625a58a3e28fa1c2f392736e74f13c48
MJLee39 <115640392+MJLee39@users.noreply.github.com>
2024-01-25 18:46:14 +0900

    [level 2] Title: [3차] 압축, Time: 7.76 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -8,32 +8,10 @@ class Solution {
         ArrayList<Integer> answerList = new ArrayList<>();
         
         HashMap<String,Integer> map = new HashMap<>();
-        map.put("A",1);
-        map.put("B",2);
-        map.put("C",3);
-        map.put("D",4);
-        map.put("E",5);
-        map.put("F",6);
-        map.put("G",7);
-        map.put("H",8);
-        map.put("I",9);
-        map.put("J",10);
-        map.put("K",11);
-        map.put("L",12);
-        map.put("M",13);
-        map.put("N",14);
-        map.put("O",15);
-        map.put("P",16);
-        map.put("Q",17);
-        map.put("R",18);
-        map.put("S",19);
-        map.put("T",20);
-        map.put("U",21);
-        map.put("V",22);
-        map.put("W",23);
-        map.put("X",24);
-        map.put("Y",25);
-        map.put("Z",26);
+        
+        for(int i=1; i<27; i++){
+            map.put(((char)(64+i))+"",i);  //알파벳 A~Z 넣기
+        }
         
         int index = 27;  //추가할 값
         
@@ -47,13 +25,13 @@ class Solution {
         		newString += arr[j]+"";
         		if(map.get(newString) == null) {
                     i=j-1;
-        			answerList.add(map.get(newString.substring(0, newString.length()-1)));
-        			map.put(newString, index);
+        			answerList.add(map.get(newString.substring(0, newString.length()-1)));  //사전에 있는 단어의 index를 정답 배열에 add
+        			map.put(newString, index);  //사전에 추가할 단어
         			index++;
         			break;
         		}
                 if(map.get(newString) != null && j == arr.length-1){
-                     flag = true;
+                     flag = true;  //여러글자로 끝날 때
                 }
         	}
             
